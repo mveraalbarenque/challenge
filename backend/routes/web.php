44d49ee5ab845b37ea1a7df04b729e17f7e673b7
Marcos A Vera A <114631005+mveraalbarenque@users.noreply.github.com>
2025-03-21 09:35:32 -0300

    Delete backend directory

--- backend/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-// use App\Http\Controllers\Api\Transacciones\GananciasController;
-
-// Route::controller(GananciasController::class)->group(function () {
-//     Route::get('/ganancias', 'index')->name('egresos.index');
-// });
